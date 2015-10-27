da0e7bd7d3ea1ab15a765637497cd09ba39db58b
Andreas Dzialocha <andreas@marmorkuchen.net>
2015-10-27 14:48:20 +0100

    simple ionic app

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -2,9 +2,49 @@
 
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', [ 'ionic', 'ui.router' ]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('index', {
+      abstract: true,
+      url: '/',
+      templateUrl: 'tabs.html'
+    })
+
+    .state('index.home', {
+      url: 'home',
+      views: {
+        'home-tab': {
+          templateUrl: 'home.html'
+        }
+      }
+    })
+
+    .state('index.test', {
+      url: 'test',
+      views: {
+        'test-tab': {
+          templateUrl: 'test.html'
+        }
+      }
+    }
+
+  );
+
+  $urlRouterProvider.otherwise('/home');
+
+});
 
 app.controller('AppCtrl', ['$scope', function($scope) {
+
+  $scope.test = 42;
+
+}]);
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
 
   $scope.test = 42;
 
